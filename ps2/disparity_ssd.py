41256d56a892342611191b5215dd63213749846d
TKratter <tomkratter@gmail.com>
2022-03-23 15:35:07 +0200

    disparity matrix is slow but working

--- ps2/disparity_ssd.py
+++ ps2/disparity_ssd.py
@@ -1,6 +1,8 @@
 import numpy as np
 from config.ps2_constants import TPL_COLS, TPL_ROWS
 from typing import Tuple
+from tqdm import tqdm
+import numba
 
 
 def disparity_ssd(left_mat: np.ndarray, right_mat: np.ndarray) -> np.ndarray:
@@ -17,27 +19,43 @@ def disparity_ssd(left_mat: np.ndarray, right_mat: np.ndarray) -> np.ndarray:
 
     disparity_matrix = np.zeros((row_size, col_size))
 
-    for r in range(row_size):
+    for r in tqdm(range(row_size)):
         for c in range(col_size):
-            disparity_matrix[r, c] = 1 / (calculate_ssd_for_reference_point(reference_point=(r, c), left_mat=left_mat,
-                                                                            right_mat=right_mat) + 0.01)
+            disparity_matrix[r, c] = calculate_disparity_for_left_reference_point(left_reference_point=(r, c),
+                                                                                  left_mat=left_mat,
+                                                                                  right_mat=right_mat)
 
     return disparity_matrix
 
 
-def calculate_ssd_for_reference_point(reference_point: Tuple[int, int], left_mat: np.ndarray,
-                                      right_mat: np.ndarray) -> float:
-    left_mat_patch = _create_valid_patch(row_start=0, row_end=TPL_ROWS, col_start=0, col_end=TPL_COLS,
-                                         mat=left_mat)
-    row_right_mat_patch_start = int(reference_point[0] - np.floor(TPL_ROWS / 2))
-    row_right_mat_patch_end = int(reference_point[0] + np.floor(TPL_ROWS / 2) + 1)
-    col_right_mat_patch_start = int(reference_point[1] - np.floor(TPL_COLS / 2))
-    col_right_mat_patch_end = int(reference_point[1] + np.floor(TPL_COLS / 2) + 1)
-    right_mat_patch = _create_valid_patch(row_start=row_right_mat_patch_start, row_end=row_right_mat_patch_end,
-                                          col_start=col_right_mat_patch_start, col_end=col_right_mat_patch_end,
-                                          mat=right_mat)
+def calculate_disparity_for_left_reference_point(left_reference_point: Tuple[int, int], left_mat: np.ndarray,
+                                                 right_mat: np.ndarray) -> int:
+    row_size, col_size = left_mat.shape
+    ssd_list = []
+    for c in range(col_size):
+        right_reference_point = (left_reference_point[0], c)
+        ssd_list.append(calculate_ssd_for_reference_points(left_reference_point=left_reference_point,
+                                                           right_reference_point=right_reference_point,
+                                                           left_mat=left_mat, right_mat=right_mat))
+
+    best_match_column = np.array(ssd_list).argmax()
+    return left_reference_point[1] - best_match_column
+
+
+def calculate_ssd_for_reference_points(left_reference_point: Tuple[int, int], right_reference_point: Tuple[int, int],
+                                       left_mat: np.ndarray, right_mat: np.ndarray) -> float:
+    left_mat_patch = _create_valid_patch(*get_patch_corners(left_reference_point), mat=left_mat)
+    right_mat_patch = _create_valid_patch(*get_patch_corners(right_reference_point), mat=right_mat)
 
     return ((left_mat_patch - right_mat_patch) ** 2).sum()
+
+
+def get_patch_corners(reference_point: Tuple[int, int]):
+    row_patch_start = int(reference_point[0] - np.floor(TPL_ROWS / 2))
+    row_patch_end = int(reference_point[0] + np.floor(TPL_ROWS / 2) + 1)
+    col_patch_start = int(reference_point[1] - np.floor(TPL_COLS / 2))
+    col_patch_end = int(reference_point[1] + np.floor(TPL_COLS / 2) + 1)
+    return row_patch_start, row_patch_end, col_patch_start, col_patch_end
 
 
 def _create_valid_patch(row_start: int, row_end: int, col_start: int, col_end: int, mat: np.ndarray) -> np.ndarray:
